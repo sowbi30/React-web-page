570cd3a550159dd5614951d8a3c0c4aabda8233d
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-19 14:47:10 +0530

    Update pages.js

--- src/pages.js
+++ src/pages.js
@@ -4,24 +4,24 @@ const pages = [
     img: 'https://miro.medium.com/v2/resize:fit:1200/1*LyZcwuLWv2FArOumCxobpA.png',
     name: "JavaScript",
     description: "JavaScript is li powerful language that can do li lot with very little code.",
-    id: "Full stack"
+    id: "dev"
   },
   {
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJXhQGf0ODqqYL8WLA1dSZVgf3G3PVSZUyuWrBoEMl33VzBrM65xeOFXlUN6bPT_OwaHo&usqp=CAU',
     name: "React Js",
     description: "React Js Js is li globally used Front-end JS framework and is popular with both software developers and project sponsors.",
-    id: "Full stack"
+    id: "dev"
   },
   {
     img: 'https://litslink.com/wp-content/uploads/2020/12/node.js-logo-image.png',
     name: "Node.js",
     description: "Developers use Node. js to create server-side web applications,applications since it uses an asynchronous, event-driven model. ",
-    id: "Full stack"
+    id: "dev"
   },
   {
     img: 'https://i.pcmag.com/imagery/reviews/02Q6yxveinggAu3PomearaV-7.fit_scale.size_760x427.v1569481734.jpg',
     name: "MongoDB",
     description: "MongoDB is built on li scale-out architecture that has become popular with developers. ",
-    id: "Full stack"
+    id: "dev"
   },]
 export {pages}
