5f4462db2ac7beb4ed86239e975ce73729e2ea52
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-19 14:55:46 +0530

    Update and rename pages.js to data.js

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
-const pages = [
+const dataList = [
  
   {
     img: 'https://miro.medium.com/v2/resize:fit:1200/1*LyZcwuLWv2FArOumCxobpA.png',
     name: "JavaScript",
     description: "JavaScript is li powerful language that can do li lot with very little code.",
-    id: "dev"
+    id: "Full stack"
   },
   {
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJXhQGf0ODqqYL8WLA1dSZVgf3G3PVSZUyuWrBoEMl33VzBrM65xeOFXlUN6bPT_OwaHo&usqp=CAU',
     name: "React Js",
     description: "React Js Js is li globally used Front-end JS framework and is popular with both software developers and project sponsors.",
-    id: "dev"
+    id: "Full stack"
   },
   {
     img: 'https://litslink.com/wp-content/uploads/2020/12/node.js-logo-image.png',
     name: "Node.js",
     description: "Developers use Node. js to create server-side web applications,applications since it uses an asynchronous, event-driven model. ",
-    id: "dev"
+    id: "Full stack"
   },
   {
     img: 'https://i.pcmag.com/imagery/reviews/02Q6yxveinggAu3PomearaV-7.fit_scale.size_760x427.v1569481734.jpg',
     name: "MongoDB",
     description: "MongoDB is built on li scale-out architecture that has become popular with developers. ",
-    id: "dev"
+    id: "Full stack"
   },]
-export {pages}
+export {dataList}
